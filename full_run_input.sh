bbc5acf4b1c2359b4ed76f09bce8b28a8a0c0bf3
IVaandrager <31886961+IVaandrager@users.noreply.github.com>
2021-02-03 13:52:05 +0100

    Update full_run_input.sh

--- full_run_input.sh
+++ full_run_input.sh
@@ -7,7 +7,7 @@
 #optional: tmux new -s peakhic
 
 conda activate peakHiC
-cd /path/to/peakHiCFolder #~/localdev/peakHiC/RUN
+cd /path/to/peakHiCFolder/RUN #~/localdev/peakHiC/RUN
 
 ##configure conf_hg38_GM12878_example_data.yml file to create appropriate peakHiCObj##
 #source("../R/peakHiC_functions.R")
@@ -15,7 +15,7 @@ cd /path/to/peakHiCFolder #~/localdev/peakHiC/RUN
 
 ##run peakHiC##
 CHROMS="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX"
-PEAKOBJS=/path/to/peakHiCObj #"~/data/Leducq/peakHiCObj/hg38_Leducq_LA_LV_12kbbins_20-70Mb.rds" 
+PEAKOBJS=/path/to/peakHiCObj #"~/data/peakHiCObj/peakHiCObj.rds" 
 
 for PEAKOBJ in $PEAKOBJS; do
 for CHR in $CHROMS; do 
